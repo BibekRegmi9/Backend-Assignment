3ef94b22c51ccdda7169683b3cb8e00889c88eb3
Bibek Regmi <1016matrixbibek@gmail.com>
2023-11-28 14:31:53 +0545

    validation added to ensure that required fields are not empty.

---
+++ src/middlewares/user-valiation.js
@@ -0,0 +1,15 @@
+const validateUserRequest = (req, res, next) => {
+    if(!req.body.name || 
+        !req.body.age||
+        !req.body.address||
+        !req.body.email
+        ){
+            return res.status(400).json({
+                success: false,
+                data: {},
+                message: 'Required fields are missing, Please enter all the required fields.',
+                err: 'Name/Age/Address/Email is missing'
+            })
+    }
+    next();
+}
