91868e82bb5bd9e0c1d04d6963db3bab9dd52009
Bibek Regmi <1016matrixbibek@gmail.com>
2023-11-28 12:40:53 +0545

    Adding Routes

--- src/index.js
+++ src/index.js
@@ -2,14 +2,18 @@ const express = require('express');
 const bodyParser = require('body-parser');
 
 
-const app = express();
 const {PORT} = require('./config/serverConfig');
+const apiRoutes = require('./routes/index');
+
+const app = express();
 
 
 const prepareAndStartServer = () => {
 
     app.use(bodyParser.json());
     app.use(bodyParser.urlencoded({extended: true}));
+
+    app.use('/', apiRoutes);
 
     app.listen(PORT, () => {
         console.log(`Server started on PORT: ${PORT}`);
